818b22510bc94f4457b7b784bd671a64969a2b75
Michael <mikey.develop@gmail.com>
2021-11-02 11:34:50 +0300

    add movieListing logic

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,17 +2,20 @@ import React, { useEffect } from 'react'
 import movieApi from '../../common/apis/MovieApi'
 import MovieListing from '../MovieListing/MovieListing'
 import { APIkey } from '../../common/apis/MovieApiKey'
+import { useDispatch } from 'react-redux'
+import { addMovies } from '../../features/movies/movieSlice'
 
 export default function Home() {
+  const movieText = 'Hunger'
+  const dispatch = useDispatch()
   useEffect(() => {
-    const movieText = 'Hunger'
     const fetchMovies = async () => {
       const response = await movieApi
         .get(`?apikey=${APIkey}&s=${movieText}&type=movie`)
         .catch((err) => {
           console.log('Err:', err)
         })
-      console.log('the response from api', response)
+      dispatch(addMovies(response.data))
     }
     fetchMovies()
   }, [])
